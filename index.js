4fb5222b52203ac0a46434fa96f3d6f797292e85
Natalia Davtyan <110120210+nataliadavtyan@users.noreply.github.com>
2022-11-15 16:30:46 +0100

    Add slider to set password length

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordLength = 15
+const passwordOne = document.getElementById("password-one")
+const passwordTwo = document.getElementById("password-two")
+const generateBtn = document.getElementById("generate-btn")
+const sliderValue = document.getElementById("slider-value")
+const sliderEl = document.getElementById("slider-el")
 
-let passwordOne = document.getElementById("password-one")
-let passwordTwo = document.getElementById("password-two")
+sliderValue.textContent = sliderEl.value
+
+// to make the number change when you dran the slider
+sliderEl.oninput = function() {
+  sliderValue.innerHTML = this.value;
+}
 
 function getRandomCharacter() {
     let randomChar = Math.floor(Math.random() * characters.length)
@@ -12,6 +20,7 @@ function getRandomCharacter() {
 }
 
 function generatePasswords() {
+    let passwordLength = sliderValue.innerHTML
     let randomPassword = ""
     for (let i = 0; i < passwordLength; i++) {
         randomPassword += getRandomCharacter()
@@ -19,7 +28,7 @@ function generatePasswords() {
     return randomPassword
 }
 
-function passwordGenerated() {
+generateBtn.addEventListener('click', function() {
     passwordOne.textContent = generatePasswords()
     passwordTwo.textContent = generatePasswords()
-}
+})
